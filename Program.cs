03fe8dc7e1c5e32026716102471eba4177b74c13
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-10-05 19:03:14 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,14 +11,51 @@ namespace Lecture_5_Logical_Operators
         static void Main(string[] args)
         {
             // Nested Ifs
-            
+  
             // Logical Operators
 
             // Constants
 
             // Ternarys
 
+            HelloWorld();
+
+            Console.WriteLine("This code just ran");
+
+            //DebuggingExample();
+
         } // Main
+        //-------------------------------------------------------------
+
+        // Method - Code Block
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World");
+        } // HelloWorld
+
+        public static void DebuggingExample()
+        {
+            // Debugging
+            // Breakpoint - Located on left hand side in grey bar
+
+            for (int i = 0; i < 30; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
 
         public static void NestedIfs()
         {
